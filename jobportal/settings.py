07053a8f8fed9a17768b06b05f28c36797cc6cf1
AFAQ <afaqalikhan331@gmail.com>
2024-02-11 15:20:49 +0500

    database

--- jobportal/settings.py
+++ jobportal/settings.py
@@ -76,8 +76,11 @@ WSGI_APPLICATION = 'jobportal.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'portal',
+        'USER' : 'root',
+        'HOST' : 'localhost',
+        'PASSWORD' : 'Afaq@123',
     }
 }
 
